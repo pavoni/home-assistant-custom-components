8aad3a38cf99f3061974d66509ff1c7e0ff8dc93
pavoni <greg@xenapto.com>
2015-09-25 21:54:24 +0100

    Fix documentation string

--- switch/custom_platform.py
+++ switch/custom_platform.py
@@ -1,5 +1,5 @@
 """
-homeassistant.components.wemo_blind
+homeassistant.components.switch
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Custom platform to import custom switches.
@@ -10,15 +10,13 @@ from custom_components.switch.custom_gate import CustomGate
 from custom_components.switch.custom_skylight import CustomSkylight
 import logging
 
-
-# DEPENDENCIES = ['switch.blind', 'switch.gate', 'switch.skylight_open', 'switch.skylight_close', 'switch.rain_sensor']
 DEPENDENCIES = []
 
 DOMAIN = "custom_platform"
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
-    """ Find and return blind switches. """
+    """ Find and return customised switches for gate, blind and skylight. """
     add_devices_callback([
         CustomBlind('Blind', 'switch.blind_toggle', hass),
         CustomGate('Gate', 'switch.gate_toggle', hass),
