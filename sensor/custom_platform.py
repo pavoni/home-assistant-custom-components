904c82d4496d7f0ae79e14324e29a8fa21a784d0
pavoni <greg@xenapto.com>
2015-09-26 11:59:16 +0100

    Remove unneeded sensor

--- sensor/custom_platform.py
+++ sensor/custom_platform.py
@@ -5,7 +5,6 @@ homeassistant.components.sensor
 Custom platform to import custom switches.
 
 """
-from custom_components.sensor.custom_sensor import CustomSensor
 from custom_components.sensor.custom_sensor_maker import CustomSensorMaker
 from custom_components.sensor.custom_sensor_insight import CustomSensorInsight
 from custom_components.sensor.custom_sensor_rain import CustomSensorRain
@@ -20,7 +19,6 @@ DOMAIN = "custom_platform_sensor"
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return custom sensors for rain_sensor and hi_fi_systemline. """
     add_devices_callback([
-        CustomSensor('Greg & Juliet', group.home, hass)
         CustomSensorMaker('Systemline', 'switch.hi_fi_systemline_sensor', hass),
         CustomSensorRain('Weather', 'switch.rain_sensor', hass),
         CustomSensorInsight('Preamp', 'switch.hi_fi_preamp', hass),
