56764d23317a8c8cdc38dcea6b009c37be422129
pavoni <greg@xenapto.com>
2015-09-26 11:48:46 +0100

    Add simple custom sensor

--- sensor/custom_platform.py
+++ sensor/custom_platform.py
@@ -5,6 +5,7 @@ homeassistant.components.sensor
 Custom platform to import custom switches.
 
 """
+from custom_components.sensor.custom_sensor import CustomSensor
 from custom_components.sensor.custom_sensor_maker import CustomSensorMaker
 from custom_components.sensor.custom_sensor_insight import CustomSensorInsight
 from custom_components.sensor.custom_sensor_rain import CustomSensorRain
@@ -19,6 +20,7 @@ DOMAIN = "custom_platform_sensor"
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return custom sensors for rain_sensor and hi_fi_systemline. """
     add_devices_callback([
+        CustomSensor('Home',group.home, hass)
         CustomSensorMaker('Systemline', 'switch.hi_fi_systemline_sensor', hass),
         CustomSensorRain('Weather', 'switch.rain_sensor', hass),
         CustomSensorInsight('Preamp', 'switch.hi_fi_preamp', hass),
