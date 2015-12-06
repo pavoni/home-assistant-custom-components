fd8490724fbf6b45315a203e8ce899359917994e
pavoni <mail@gregdowling.com>
2015-12-06 17:37:34 +0000

    Ignore change when already in that state

--- switch/custom_gate.py
+++ switch/custom_gate.py
@@ -7,10 +7,14 @@ class CustomGate(CustomWemoMaker):
 
     def turn_on(self, **kwargs):
         """ Turns the switch on. """
+        if self.is_on :
+        	return
         core.turn_on(self.hass, self.target)
         super(CustomGate, self).turn_on()
 
     def turn_off(self):
         """ Turns the switch off. """
+        if not self.is_on :
+        	return
         core.turn_on(self.hass, self.target)
         super(CustomGate, self).turn_off()
