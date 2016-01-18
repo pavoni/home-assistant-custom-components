862efae9d608f962f93f8e6555f9741d20b29e98
pavoni <mail@gregdowling.com>
2016-01-18 17:33:48 +0000

    Revise gate retry.

--- switch/custom_gate.py
+++ switch/custom_gate.py
@@ -2,6 +2,10 @@ from custom_components.switch.custom_wemo_maker import CustomWemoMaker
 
 import homeassistant.components as core
 
+# Press open button twice to avoid partial open issue
+REPRESS_AFTER 25
+REPRESS_GAP 1.5
+
 class CustomGate(CustomWemoMaker):
     """ Wraps an exising switch, changing behaviour. """
 
@@ -10,6 +14,11 @@ class CustomGate(CustomWemoMaker):
         if self.is_on :
         	return
         core.turn_on(self.hass, self.target)
+        if not REPRESS_AFTER is NONE:
+            sleep(REPRESS_AFTER)
+            core.turn_on(self.hass, self.target)
+            sleep(REPRESS_GAP)
+            core.turn_on(self.hass, self.target)
         super(CustomGate, self).turn_on()
 
     def turn_off(self):
