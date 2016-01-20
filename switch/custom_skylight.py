885dde8d2d112913fefa623b1d157debd3f94f6b
pavoni <mail@gregdowling.com>
2016-01-20 11:13:26 +0000

    Tidy senor code.

--- switch/custom_skylight.py
+++ switch/custom_skylight.py
@@ -3,7 +3,12 @@ import logging
 from homeassistant.components.switch import SwitchDevice
 
 import homeassistant.components as core
+from homeassistant.const import (
+    STATE_ON, STATE_OFF)
+
 from homeassistant.helpers.event import track_state_change
+
+STATE_FAILED = 'failed'
 
 # Shortcut for the logger
 _LOGGER = logging.getLogger(__name__)
@@ -17,9 +22,8 @@ class CustomSkylight(SwitchDevice):
         self.target_off = maker_off
         self.target_rain_sensor = maker_rain_sensor
         self.hass = new_hass
-        # Keep state private to avoid anything else changing it!
         # Default to off - no sensor - but probably closed
-        self._state = 'off'
+        self._state = STATE_OFF
 
         def close_skylight_if_raining(entity_id, old_state, new_state):
             """ Called when the target device changes state. """
@@ -43,18 +47,18 @@ class CustomSkylight(SwitchDevice):
     def raining(self):
         """ Returns whether the rain sensor think it's raining. Default to True if there is a problem!"""
         try:
-            sensor_state = self.hass.states.get(self.target_rain_sensor).attributes.get('sensor_state', 'failed')
+            sensor_state = self.hass.states.get(self.target_rain_sensor).attributes.get('sensor_state', STATE_FAILED)
         except AttributeError:
-            sensor_state = 'failed'
+            sensor_state = STATE_FAILED
             _LOGGER.warning('CustomSkylight could not comminucate with rain sensor!')
-        return not (sensor_state == 'off')
+        return not (sensor_state == STATE_OFF)
 
 
     def turn_on(self, **kwargs):
         """ Opens the Skylight - toggles the momentary switch. """
         if not self.raining:
             core.turn_on(self.hass, self.target_on)
-            self._state = 'on'
+            self._state = STATE_ON
         else:
             _LOGGER.warning('CustomSkylight open - refused because of rain!')
 
@@ -62,4 +66,4 @@ class CustomSkylight(SwitchDevice):
     def turn_off(self):
         """ Closes the Skylight - toggles the momentary switch. """
         core.turn_on(self.hass, self.target_off)
-        self._state = 'off'
+        self._state = STATE_OFF
